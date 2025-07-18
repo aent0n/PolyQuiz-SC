922141387205a10e104eccc25d929b566c0006d1
Anton ADAM <antonethan2014@gmail.com>
2025-07-18 11:51:59 +0000

    non... toujours pas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -19,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fr" className={`dark ${inter.variable}`}>
-      <body className="antialiased font-sans">
+    <html lang="fr" className="dark">
+      <body className={cn("antialiased font-sans", inter.variable)}>
         {children}
         <Toaster />
       </body>
